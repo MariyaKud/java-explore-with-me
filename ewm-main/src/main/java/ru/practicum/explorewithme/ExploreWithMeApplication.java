9af4cb5717b89cc0174d807c781d636c9825bc61
MariyaKud <m.kudryashova@tealfox.com>
2023-11-13 09:05:20 +0300

    #Diplom level2 for first review check 3 add delay

--- ewm-main/src/main/java/ru/practicum/explorewithme/ExploreWithMeApplication.java
+++ ewm-main/src/main/java/ru/practicum/explorewithme/ExploreWithMeApplication.java
@@ -3,10 +3,13 @@ package ru.practicum.explorewithme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class ExploreWithMeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(10);
 		SpringApplication.run(ExploreWithMeApplication.class, args);
 	}
 }
