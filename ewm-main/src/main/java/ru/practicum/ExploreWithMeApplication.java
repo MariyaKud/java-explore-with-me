b2eef86b9e00bcd9b5b0a99297efcde08b8e7570
MariyaKud <m.kudryashova@tealfox.com>
2023-11-13 16:25:18 +0300

    #Diplom level2 for first review check 7 depends

--- ewm-main/src/main/java/ru/practicum/ExploreWithMeApplication.java
+++ ewm-main/src/main/java/ru/practicum/ExploreWithMeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExploreWithMeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ExploreWithMeApplication.class, args);
 	}
 }
